3951f36ba6dd624865ceb0bf1025df264ad609f8
Rokas111 <sonic.rokas@gmail.com>
2020-03-08 17:52:17 +0200

    ro

--- src/me/Rokaz/BlueLight/core/cmd/embeds/PopUpEmbed.java
+++
@@ -1,9 +0,0 @@
-package me.Rokaz.BlueLight.core.cmd.embeds;
-
-import me.Rokaz.BlueLight.core.lib.embed.premades.SimpleCustomEmbed;
-
-public class PopUpEmbed extends SimpleCustomEmbed {
-    public PopUpEmbed(String error) {
-        super(error,true);
-    }
-}
--- src/me/Rokaz/BlueLight/core/lib/messages/reactions/ReactionExecutor.java
+++
@@ -1,8 +0,0 @@
-package me.Rokaz.BlueLight.core.lib.messages.reactions;
-
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-
-public interface ReactionExecutor {
-    void executeReaction(Message m, Member p);
-}
